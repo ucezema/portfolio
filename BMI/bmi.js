8210ae7172a425c679c6bc3d2703e4d5a0d5527c
ucezema <ucezema@yahoo.com>
2022-05-21 00:10:02 +0100

    Updated my BMI Javascript file and linked bmi.html to index.html

--- BMI/bmi.js
+++ BMI/bmi.js
@@ -1,42 +1,29 @@
 
-// var weight = parseInt(document.getElementById("weight").value);
-// var height = parseInt(document.getElementById("height").value);
-var weight = 85;
-var height = 2;
+let weight = 85; //parseInt(document.getElementById("weight").value);
+let height = 175; //parseInt(document.getElementById("height").value);
+let bmi;
+let bmiResult;
 
-var heightSquare = Math.pow(height,2);
-var myBMI = document.querySelector("#bmi");
-var bmi = calcBMI(weight, heightSquare);
-var bmi2 = Math.round(bmi);
-var bmiResult;
-var btn = document.querySelector("#btn1");
+let btn = document.querySelector("#btn");
+console.log(btn);
 
-function calcBMI(a, b) {
-    return a / b;
-}
+btn.addEventListener('click', calcBMI);
+
+function calcBMI() {
+
+    bmi = weight/((height/100) * (height/100));
+    bmiResult = document.querySelector("#bmi").innerHTML;
+
     if (bmi < 18.5) {
-        bmiResult = "BMI is: " + bmi2 + " meaning, you're underweight.";
+        bmiResult = "BMI is: " + bmi + " meaning, you're underweight."
     }   else if (bmi < 25) {
-        bmiResult = "BMI is: " + bmi2 + " meaning, you're healthy.";
+        bmiResult = "BMI is: " + bmi + " meaning, you're healthy."
     }   else if (bmi < 30) {
-        bmiResult = "BMI is: " + bmi2 + " meaning, you're overweight.";
+        bmiResult = "BMI is: " + bmi + " meaning, you're overweight."
     }   else if (bmi > 30) {
-        bmiResult = "BMI is: " + bmi2 + " meaning, you're obese.";
+        bmiResult = "BMI is: " + bmi + " meaning, you're obese."
     }   else {
-        bmiResult = "BMI is: " + bmi2 + " meaning, your result is invalid.";
+        bmiResult = "BMI is: " + bmi + " meaning, your result is invalid."
+    }
 }
-
-btn.addEventListener('click', function() {
-    document.getElementById("dem").innerHTML = "Height square is: " + heightSquare;
-    myBMI.innerHTML = bmiResult;
-});
-
-// btn.addEventListener('click', displayResult());
-// document.getElementById("bmi").innerHTML = calcBMI(x, z);
-
-
-
-
-// document.getElementById("serv").innerHTML = "Available services";
-// document.querySelector(".service").style.color="#24A19C";
 
